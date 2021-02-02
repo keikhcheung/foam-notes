c0c3a9bd1409b6ab304726fe7e67645f6cc2168f
kei <kei.khcheung@gmail.com>
2021-02-03 00:41:19 +0100

    Gatsby shadowing + modules

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: ``,
   siteMetadata: {
-    title: `Foam`,
+    title: `kei’s notes`,
   },
   plugins: [
     {
       resolve: `gatsby-theme-garden`,
       options: {
         rootNote: "/readme",
-        contentPath: `${__dirname}/..`,
+        contentPath: `${__dirname}/notes/..`,
         ignore: [
           "**/_layouts/**",
           "**/.git/**",
@@ -17,5 +17,29 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [
+          {
+            resolve: "gatsby-remark-smartypants",
+            options: {
+              dashes: "oldschool",
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-favicon`,
+    `gatsby-plugin-react-helmet`
   ],
 };
