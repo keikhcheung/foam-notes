294e90d63bb74424347b429bd62aa61e26209b9b
kei <kei.khcheung@gmail.com>
2021-02-03 01:19:58 +0100

    test deploy

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -17,29 +17,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [
-          {
-            resolve: "gatsby-remark-smartypants",
-            options: {
-              dashes: "oldschool",
-            },
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-favicon`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-favicon`
   ],
 };
