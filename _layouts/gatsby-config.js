1960329ada2c61a3a8d7fc457bf724012b3867f5
kei <kei.khcheung@gmail.com>
2021-02-04 23:33:18 +0100

    try notes folder

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/notes`,
+  pathPrefix: ``,
   siteMetadata: {
     title: `kei’s notes`,
   },
@@ -8,7 +8,7 @@ module.exports = {
       resolve: `gatsby-theme-garden`,
       options: {
         rootNote: "/readme",
-        contentPath: `${__dirname}/notes/..`,
+        contentPath: `${__dirname}/../notes`,
         ignore: [
           "**/_layouts/**",
           "**/.git/**",
