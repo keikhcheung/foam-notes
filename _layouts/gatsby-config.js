d933ead38c91776706d15652a53f15f8a96a8cd6
kei <kei.khcheung@gmail.com>
2021-02-05 00:02:54 +0100

    about

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-theme-garden`,
       options: {
-        rootNote: "/notes/about",
+        rootNote: "/about",
         contentPath: `${__dirname}/../notes`,
         ignore: [
           "**/_layouts/**",
